1d9c7c9fd6aa0261b87fc4f79901fdec16053b0f
San Lamamba P <contact@sanlamamba.com>
2023-01-20 12:33:26 +0000

    Spinning disc animation

--- frontend/src/pages/UserPage/Projet/index.jsx
+++ frontend/src/pages/UserPage/Projet/index.jsx
@@ -3,14 +3,14 @@ import ProjetBox from "../../../components/Projet/ProjetBox";
 
 export default function index() {
   return (
-    <div className="container-fluid pt-2 projet__container mb-4 d-flex justify-content-center align-items-center">
+    <div className="container-fluid pt-2 projet__container mb-4 d-flex justify-content-center align-items-center projet_container">
       <div className="row">
         <div className="col-md-9 col-12 px-2">
           <ProjetBox />
         </div>
         <img
           src="/assets/images/vinyle.png"
-          className="projet__disc d-none d-md-block"
+          className="projet__disc spin d-none d-md-block"
           alt="mams vinyle"
         />
       </div>
