19b5a3993eb3070521710afd79678332819ecb5b
San Lamamba P <62671141+sanlamamba@users.noreply.github.com>
2023-02-03 13:45:14 +0000

    contact img

--- frontend/src/pages/UserPage/Contact/index.jsx
+++ frontend/src/pages/UserPage/Contact/index.jsx
@@ -5,7 +5,7 @@ export default function index() {
   return (
     <div className="contact_container">
       <div className="row">
-        <div className="col-md-5 col-0 d-none d-md-block px-2 contact__img">
+        <div className="col-md-5 col-0 px-2 contact__img">
           <img src="/assets/images/contact-background.png" alt="" srcset="" />
         </div>
         <div className="col-md-7 col-12 form__container">
